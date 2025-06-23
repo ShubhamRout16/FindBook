ef5e369883e6fec1290a1a655f1bfc597bcd34ad
ShubhamRout16 <routshubham2028@gmail.com>
2025-06-23 23:35:08 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,223 @@
+// since data is not accessible to saveBook function we will make a global variable to save data which can be accessed by both functions
+let bookData = []
+let removeBook // for capturing the removing book id
+async function getBook(){
+  // store the user sent input
+  const bookName = document.getElementById('bookNameInput').value.trim()
+  // console.log(bookName);
+  
+  if(!bookName){
+    alert('Please enter a bookname')
+    return
+  }
+  // url of the api integrated with users bookname
+  // bug -> gap in book name causes error -> encodeURIComponent
+  const url = `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(bookName)}`
+  // console.log(url);
+  
+
+  //try block
+  try{
+    // response recieved from fetching the api
+    const response = await fetch(url) // asynchronous task which will take time
+    // handling the response
+    if(!response.ok){
+      throw new Error('response not found')
+    }
+    // the actual data recieved by api
+    const data = await response.json()
+    bookData = data.items ; // storing data.items so it can accessed by saveBook function and changes made in global level
+    console.log(data);
+    console.log(data.items[0].volumeInfo.title); // volume 1 title
+    console.log(data.items[0].volumeInfo.publishedDate); // volume 1 publishedDate
+    console.log(data.items[0].volumeInfo.publisher); // volume 1 publisher
+    console.log(data.items[0].volumeInfo.pageCount); // volume 1 pageCount
+    //bug -> all books doesnt have the same author field & some books have more than one author
+    console.log(data.items[1].volumeInfo.authors.length);
+    console.log(data.items.length);
+    
+    // ui updation
+    // const bookShower = document.getElementById('bookDetailsShower')
+    // bookShower.innerHTML = `
+    //   <img src = "${data.items[0].volumeInfo.imageLinks.smallThumbnail}" alt = "books image" />
+    //   <h3>Book title : ${data.items[0].volumeInfo.title}</h3>
+    //   <p><strong>Author : ${data.items[0].volumeInfo.authors[0]}</strong></p>
+    //   <p><strong>Page Count : ${data.items[0].volumeInfo.pageCount}</strong></p>
+    //   <p><strong>Published Date : ${data.items[0].volumeInfo.publishedDate}</strong></p>
+    //   <p><strong>Publisher : ${data.items[0].volumeInfo.publisher}</strong></p>
+    // `
+
+    let bookShower = document.getElementById('bookDetailsShower')
+    // showing multiple books at a single time
+    for(let i=0; i<data.items.length; i++){
+      // all volume titles
+      // console.log(data.items[i].volumeInfo.title); 
+      // all volume publishedDate
+      // console.log(data.items[i].volumeInfo.publishedDate);
+      // all volume publisher 
+      // console.log(data.items[i].volumeInfo.publisher); 
+      // all volume page counts
+      // console.log(data.items[i].volumeInfo.pageCount); 
+      // all volume thumbnail
+      // lets show it on the dom
+      // bug -> only one image shows up on dom because .innerHTML replaces current element so we have to append using + operator
+      // console.log(data.items[i].volumeInfo.imageLinks.smallThumbnail);
+      
+      let bookCard = `
+      <div class="book-card">
+      <button id="saveBook" onclick="saveBook(${i})">Save</button> 
+      <br>
+      <img src = "${data.items[i].volumeInfo.imageLinks.smallThumbnail}" alt = "books image" />
+      <h3>Book title : ${data.items[i].volumeInfo.title}</h3>
+      <p><strong>Page Count : ${data.items[i].volumeInfo.pageCount || 'N/A'}</strong></p>
+      <p><strong>Published Date : ${data.items[i].volumeInfo.publishedDate || 'N/A'}</strong></p>
+      <p><strong>Publisher : ${data.items[i].volumeInfo.publisher || 'N/A'}</strong></p>
+      
+      `
+      
+      // bug -> some books doesnt have authors so error is thrown
+      if(data.items[i].volumeInfo.authors){
+        for(let j=0; j<data.items[i].volumeInfo.authors.length; j++){
+          // console.log(data.items[i].volumeInfo.authors[j]);
+          // if(data.items[i].volumeInfo.authors.length > 1){
+          //   console.log(data.items[i].volumeInfo.authors[j])
+          //   bookShower.innerHTML += `
+          //   <p><strong>${console.log(data.items[i].volumeInfo.authors[j])}</strong></p>
+          //   `
+          // }else{
+          //   console.log(data.items[i].volumeInfo.authors[j])
+          // }
+          bookCard += `
+            <p><strong>Authors : ${(data.items[i].volumeInfo.authors[j])}</strong></p>
+            `
+        }
+      }
+      bookCard += `</div>`;
+      bookShower.innerHTML += bookCard;
+    }
+  }
+  // catch block for errors
+  catch(error){
+    const bookShower = document.getElementById('bookDetailsShower')
+    bookShower.innerHTML = `
+    <p style="backgroundColor: red textColor: white">Error is : ${error.message}</p>
+    `
+  }
+}
+
+// creating Save button to save to the LocalStorage
+
+// step 1 -> getting the bookdata and saving it in the localStorage
+function saveBook(index){
+  console.log(index);
+  let toSaveBook = bookData[index] // getting the book which user wants to store
+  // we got book data -> get the exisiting saved Books from localStorage
+  let saved = JSON.parse(localStorage.getItem('savedBooks')) || [] // fetches saved book if there is or empty array & JSON.parse() converts string to array
+  // now check for if there is any book matching to the one we are going to save -> to prevent duplicate saving
+  let alreadyExists = saved.some((existingBook) => { // .some method checks if any one of the element meets the condition and returns true & false based on that
+    return existingBook.id === toSaveBook.id
+  })
+  // if there is no duplicate book push new book into the saved array
+  if(!alreadyExists){
+    saved.push(toSaveBook)
+    localStorage.setItem('savedBooks',JSON.stringify(saved)) // we must convert the array back into strings because localStorage only stores strings
+    showToast("Book saved!");
+  }else{
+    showToast("Book already saved!");
+  }
+  // save the updated array back to localStorage
+  
+}
+
+// step 2 -> get the saved book data from the localStorage and display it in the DOM
+function showSavedBooks(){
+  let saved = JSON.parse(localStorage.getItem('savedBooks')) || []
+  // now loop through the saved books and display it in the dom
+  // bug -> for removeOnly() (more books appearing when you click remove) this happens because we are not cleaning the previous dom before rendering updated saved books list
+  const savedBooks = document.getElementById('savedBooks')
+  savedBooks.innerHTML = ''
+  saved.forEach(book => {
+    console.log(book);
+    
+    // removeBook = book
+    // console.log(book.volumeInfo.title);
+    savedBooks.innerHTML += `
+    <div class="book-card">
+    <img src = "${book.volumeInfo.imageLinks.smallThumbnail}" alt = "books image" />
+    <h3>Book title : ${book.volumeInfo.title}</h3>
+    <p><strong>Authors : ${book.volumeInfo.authors?.join(', ') || 'unknown author'}</strong></p>
+    <button id="removeOnly" onclick="removeOnly('${book.id}')" style="margin-bottom: 10px;">Remove</button>
+    <br>
+    </div>
+    `
+  });
+}
+
+
+// removing all the savedbook from localstorage
+// why prefer to use .removeItem instead of .clear ?
+// .clear() removes everything from localStorage including scripts etx
+function clearAll(){
+  localStorage.removeItem('savedBooks')
+  // problem -> after removing savedBooks from the localStorage UI is not updated
+  const savedBooks = document.getElementById('savedBooks')
+  savedBooks.innerHTML = ''
+  showToast("All books cleared!");
+}
+
+// feature to only remove the selected items
+function removeOnly(removeBook){
+  let saved = JSON.parse(localStorage.getItem('savedBooks')) || []
+  let updated = saved.filter((filteredBook) => {
+    return filteredBook.id !== removeBook
+  })
+  localStorage.setItem('savedBooks',JSON.stringify(updated))
+  showSavedBooks();
+  showToast("Book removed!");
+}
+
+// toast notification after save and remove
+function showToast(message) {
+  const toast = document.getElementById('toast');
+  toast.textContent = message;
+  toast.style.display = 'block';
+
+  setTimeout(() => {
+    toast.style.display = 'none';
+  }, 2000);
+}
+
+// showing the saved books when page reloads
+window.onload = function() {
+  showSavedBooks();
+
+  // adding persistance to the dark mode
+  if(localStorage.getItem('darkMode') === 'true'){
+    document.body.classList.add('dark-mode')
+  }
+
+  updateToggleText();
+}
+
+// adding dark mode toggle feature
+document.getElementById('toggleDarkMode').addEventListener('click' , () => {
+  console.log("clicked");
+  
+  // will use toggle method
+  // checks if -> there is class name dark-mode on body 
+  // if there isnt adds it and if there is then removes it
+  document.body.classList.toggle('dark-mode') 
+  // .contains check if there is className dark-mode and returns true and false based on that
+  const isDark = document.body.classList.contains('dark-mode')
+  localStorage.setItem('darkMode',isDark) // saving the result true and false to the localStorage
+
+  // bug : text changes only when the page refreshes to make changes when the button is clicked we need call it here also
+  updateToggleText();
+})
+
+// changing button text and icon when changing between light and dark mode
+function updateToggleText(){
+  const btn = document.getElementById('toggleDarkMode')
+  const isDark = document.body.classList.contains('dark-mode')
+  btn.textContent = isDark ? '☀️ Light Mode' : '🌙 Dark Mode';
+}
